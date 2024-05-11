31dece013b37d3bdd305c23b2d5ca2f720beeb72
gyuehsiwj <gs1iamdanya@hotmail.com>
2024-05-11 15:35:02 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-function isValidSudoku(board) {
-  const rows = new Array(9).fill().map(() => new Array(9).fill(0));
-  const cols = new Array(9).fill().map(() => new Array(9).fill(0));
-  const boxes = new Array(9).fill().map(() => new Array(9).fill(0));
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      if (board[i][j] !== ".") {
-        const num = Number(board[i][j]) - 1;
-        const k = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-        if (rows[i][num] || cols[j][num] || boxes[k][num]) return false;
-        rows[i][num] = 1;
-        cols[j][num] = 1;
-        boxes[k][num] = 1;
-      }
+function multiply(num1, num2) {
+  const m = num1.length;
+  const n = num2.length;
+  const pos = new Array(m + n).fill(0);
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      const mul = (num1[i] - "0") * (num2[j] - "0");
+      const p1 = i + j;
+      const p2 = i + j + 1;
+      const sum = mul + pos[p2];
+      pos[p1] += Math.floor(sum / 10);
+      pos[p2] = sum % 10;
     }
   }
-  return true;
+  let result = "";
+  for (const p of pos) {
+    if (!(result.length === 0 && p === 0)) result += p;
+  }
+  return result.length === 0 ? "0" : result;
 }
